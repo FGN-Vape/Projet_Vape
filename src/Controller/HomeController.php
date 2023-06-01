0ad2764d5a2706739cf6dd1ac2e272873ecd3090
AlexAnthony13003 <alexisguillermain.pro@gmail.com>
2023-06-01 13:00:01 +0200

    🎨Mise en place front

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
@@ -17,5 +19,25 @@ class HomeController extends AbstractController
         public function shop(): Response
     {
         return $this->render('pages/shop.html.twig');
+    }
+    #[Route('/produit', 'product.index', methods: ['GET', 'POST'])]
+        public function product(EntityManagerInterface $manager): Response
+    {
+        // Récupérer l'identifiant du produit à partir de la base de données
+        $produitId = 2;
+        // Récupérer le produit à partir de l'identifiant
+        $produit = $manager->getRepository(Product::class)->find($produitId);
+
+        return $this->render('pages/product.html.twig', [
+            'produit' => $produit,
+        ]);
+    }
+    #[Route('/liste', 'list_products.index', methods: ['GET'])]
+        public function liste(EntityManagerInterface $manager): Response
+    {
+        $lesProduits = $manager->getRepository(Product::class)->findAll();
+        return $this->render('pages/list_products.html.twig', [
+            'produits' => $lesProduits,
+        ]);
     }
 }
