3bb5228cbe61efa9e3f437b47e8ac2d3e082733a
Retiks <lucas.navarro.pro@gmail.com>
2023-06-06 02:27:38 +0200

    Ajout Admin

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Product;
+use App\Form\AddProductType;
+use App\Form\UpdateProductType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
+
+class AdminController extends AbstractController
+{
+    private $slugger;
+    public function __construct(SluggerInterface $slugger)
+    {
+        $this->slugger = $slugger;
+    }
+
+    #[Route('/admin', name: 'app_admin')]
+    public function index(EntityManagerInterface $manager): Response
+    {
+        $lesProduits = $manager->getRepository(Product::class)->findAll();
+        return $this->render('admin/index.html.twig', [
+            'controller_name' => 'AdminController',
+            'produits' => $lesProduits
+        ]);
+    }
+
+    #[Route('/admin/ajoutProduit', name: 'ajoutProduit')]
+    public function ajoutProduit(EntityManagerInterface $manager, Request $request): Response
+    {
+        $product = new Product();
+        $this->denyAccessUnlessGranted("ROLE_ADMIN");
+        $form = $this->createForm(AddProductType::class, $product);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()){
+            $product = $form->getData();
+            $uploadedFile = $form['img']->getData();
+            $destination = $this->getParameter('kernel.project_dir').'/public/img';
+            $originalFilename = pathinfo($uploadedFile->getClientOriginalName(), PATHINFO_FILENAME);
+            $newFilename =  $originalFilename.'-'.uniqid().'.'.$uploadedFile->guessExtension();
+
+            $uploadedFile->move(
+                $destination,
+                $newFilename
+            );
+            $product->setImg('/img/'.$newFilename);
+            $this->addFlash(
+                'success',
+                'Votre Produit a bien été créé'
+            );
+
+            $manager->persist($product);
+            $manager->flush();
+
+            return $this->redirectToRoute('list_products.index');
+        }
+
+        return $this->render('admin/ajoutProduit.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    #[Route('/admin/remove/{id}', name: 'removeProduct')]
+    public function removeProduct(EntityManagerInterface $manager, int $id): Response
+    {
+        $this->denyAccessUnlessGranted("ROLE_ADMIN");
+        $produit = $manager->getRepository(Product::class)->findOneBy(["id" => $id]);
+        $manager->remove($produit);
+        $manager->flush();
+        $this->addFlash(
+            'success',
+            'Votre Produit a bien été supprimé'
+        );
+        return $this->redirectToRoute('app_admin');
+    }
+
+    #[Route('/admin/update/{id}', name: 'updateProduct')]
+    public function updateProduct(EntityManagerInterface $manager, int $id, Request $request): Response
+    {
+        $produit = $manager->getRepository(Product::class)->findOneBy(["id" => $id]);
+        $this->denyAccessUnlessGranted("ROLE_ADMIN");
+        $form = $this->createForm(UpdateProductType::class, $produit);
+        $form->handleRequest($request);
+        if ($form->isSubmitted()&& $form-> isValid()) {
+            $produit = $form->getData();
+            $manager->persist($produit);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                'Votre Produit a été modifié avec succès'
+            );
+
+            return $this->redirectToRoute('app_admin');
+        }
+
+        return $this->render('admin/updateProduct.html.twig', [
+            'form' =>$form->createView()
+        ]);
+    }
+}
---
+++ src/Form/UpdateProductType.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Product;
+use App\Entity\Type;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UpdateProductType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('Name', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'minlength' => '2',
+                    'maxlength' => '10'
+                ],
+                'label' => 'Nom : ',
+                'label_attr' => [
+                    'class' => 'form-label mt-4'
+                ]
+            ])
+            ->add('Brand', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'minlength' => '2',
+                    'maxlength' => '10'
+                ],
+                'label' => 'Marque : ',
+                'label_attr' => [
+                    'class' => 'form-label mt-4'
+                ]
+            ])
+            ->add('Price', MoneyType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'minlength' => '2',
+                    'maxlength' => '10',
+                ],
+                'label' => 'Prix : ',
+                'label_attr' => [
+                    'class' => 'form-label mt-4'
+                ]
+            ])
+            ->add('description', TextareaType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'minlength' => '2',
+                    'maxlength' => '10'
+                ],
+                'label' => 'Description : ',
+                'label_attr' => [
+                    'class' => 'form-label mt-4'
+                ]
+            ])
+            ->add('Type', EntityType::class, [
+                'class' => Type::class,
+                'choice_label' => 'nametype',
+                'choice_value' => 'id',
+                'placeholder' => 'Choisis un type',
+                'attr' => [
+                    'class' => 'form-control',
+                    'minlength' => '2',
+                    'maxlength' => '10'
+                ],
+                'label' => 'Type : ',
+                'label_attr' => [
+                    'class' => 'form-label mt-4'
+                ]
+            ])            ->add('submit', SubmitType::class, [
+                'attr' => [
+                    'class' => 'btn btn-primary mt-4'
+                ],
+                'label' => 'Envoyer'
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Product::class,
+        ]);
+    }
+}
